b65213be82bb721d2cf9de673961c728a8fd2535
HSBODo <vinsulill@gmaile.com>
2023-12-15 23:10:23 +0900

    test : S3fileService 테스트코드 작성

--- src/test/java/site/pointman/chatbot/service/S3FileServiceTest.java
+++ src/test/java/site/pointman/chatbot/service/S3FileServiceTest.java
@@ -1,11 +1,15 @@
 package site.pointman.chatbot.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import site.pointman.chatbot.dto.product.ProductImageDto;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
@@ -13,11 +17,23 @@ import static org.junit.jupiter.api.Assertions.*;
 class S3FileServiceTest {
     @Autowired
     S3FileService s3FileService;
+    private final String S3_REPOSITORY ="https://pointman-file-repository.s3.ap-northeast-2.amazonaws.com/";
+
 
     @Test
-    void upload() {
-        File file = new File("src/main/resources/image/온도3");
-        String url = s3FileService.upload(file, "image");
-        log.info("url={}",url);
+    void uploadProductImage() {
+        //give
+        List<String> imageUrls = new ArrayList<>();
+        String imageUrl = "https://pointman-file-repository.s3.ap-northeast-2.amazonaws.com/image/%ED%85%8C%EC%8A%A4%ED%8A%B8.jpg";
+        String userKey = "테스트유저키";
+        String productName = "테스트상품명";
+        imageUrls.add(imageUrl);
+
+        //when
+        ProductImageDto productImageDto = s3FileService.uploadProductImage(imageUrls, userKey, productName);
+        List<String> returnImageUrl = productImageDto.getImageUrl();
+
+        //then
+        Assertions.assertThat(returnImageUrl.get(0)).contains(S3_REPOSITORY);
     }
 }
