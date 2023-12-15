01ceb6e6b83bba91ac5d3401f4436cb244973e3c
HSBODo <vinsulill@gmaile.com>
2023-12-16 01:17:02 +0900

    test : @BeforeEach 변수 초기화

--- src/test/java/site/pointman/chatbot/service/LogServiceTest.java
+++ src/test/java/site/pointman/chatbot/service/LogServiceTest.java
@@ -21,6 +21,7 @@ class LogServiceTest {
 
     ObjectMapper mapper = new ObjectMapper();
     ChatBotRequest chatBotRequest;
+
     @BeforeEach
     public void setUp() throws JsonProcessingException {
         String body="{\"bot\":{\"id\":\"65262a2d31101d1cb1106082!\",\"name\":\"중계나라\"},\"intent\":{\"id\":\"657bc60c11c58311fd9c9fa4\",\"name\":\"테스트\",\"extra\":{\"reason\":{\"code\":1,\"message\":\"OK\"}}},\"action\":{\"id\":\"657bc62306b53a111f7c354e\",\"name\":\"테스트_Json\",\"params\":{},\"detailParams\":{},\"clientExtra\":{}},\"userRequest\":{\"block\":{\"id\":\"657bc60c11c58311fd9c9fa4\",\"name\":\"테스트\"},\"user\":{\"id\":\"bf1409542da67b26c2262a0a2f72ac6b5df6ad45e49e90543bd4586af560622863\",\"type\":\"botUserKey\",\"properties\":{\"botUserKey\":\"bf1409542da67b26c2262a0a2f72ac6b5df6ad45e49e90543bd4586af560622863\",\"isFriend\":true,\"plusfriendUserKey\":\"QFJSyeIZbO77\",\"bot_user_key\":\"bf1409542da67b26c2262a0a2f72ac6b5df6ad45e49e90543bd4586af560622863\",\"plusfriend_user_key\":\"QFJSyeIZbO77\"}},\"utterance\":\"테스트\",\"params\":{\"surface\":\"Kakaotalk.plusfriend\"},\"lang\":\"ko\",\"timezone\":\"Asia/Seoul\"},\"contexts\":[]}\n";
--- src/test/java/site/pointman/chatbot/service/ProductServiceTest.java
+++ src/test/java/site/pointman/chatbot/service/ProductServiceTest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -30,11 +31,18 @@ class ProductServiceTest {
 
     private ObjectMapper mapper = new ObjectMapper();
     private ChatBotRequest chatBotRequest;
+    private String useKey;
+    private String productId;
+
+    @BeforeEach
+    public void setUp() throws JsonProcessingException {
+        useKey = "QFJSyeIZbO77";
+        productId = "5942";
+    }
 
     @Test
     @Transactional
     void validationCustomer() throws Exception{
-        String useKey = "QFJSyeIZbO77";
         String body="{\"bot\":{\"id\":\"65262a2d31101d1cb1106082!\",\"name\":\"중계나라\"},\"intent\":{\"id\":\"657bc60c11c58311fd9c9fa4\",\"name\":\"테스트\",\"extra\":{\"reason\":{\"code\":1,\"message\":\"OK\"}}},\"action\":{\"id\":\"657bc62306b53a111f7c354e\",\"name\":\"테스트_Json\",\"params\":{},\"detailParams\":{},\"clientExtra\":{}},\"userRequest\":{\"block\":{\"id\":\"657bc60c11c58311fd9c9fa4\",\"name\":\"테스트\"},\"user\":{\"id\":\"bf1409542da67b26c2262a0a2f72ac6b5df6ad45e49e90543bd4586af560622863\",\"type\":\"botUserKey\",\"properties\":{\"botUserKey\":\"bf1409542da67b26c2262a0a2f72ac6b5df6ad45e49e90543bd4586af560622863\",\"isFriend\":true,\"plusfriendUserKey\":\""+useKey+"\",\"bot_user_key\":\"bf1409542da67b26c2262a0a2f72ac6b5df6ad45e49e90543bd4586af560622863\",\"plusfriend_user_key\":\"QFJSyeIZbO77\"}},\"utterance\":\"테스트\",\"params\":{\"surface\":\"Kakaotalk.plusfriend\"},\"lang\":\"ko\",\"timezone\":\"Asia/Seoul\"},\"contexts\":[]}\n";
         chatBotRequest = mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false)
                 .readValue(body, ChatBotRequest.class);
@@ -52,7 +60,7 @@ class ProductServiceTest {
         chatBotRequest = mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false)
                 .readValue(body, ChatBotRequest.class);
         //when
-        ChatBotResponse chatBotResponse = productService.createProductInfoPreview(chatBotRequest);
+        ChatBotResponse chatBotResponse = productService.getProductInfoPreview(chatBotRequest);
         String text = chatBotResponse.getTemplate().getOutputs().get(1).getTextCard().getTitle();
 
         //then
@@ -80,7 +88,6 @@ class ProductServiceTest {
     @Transactional
     void getCustomerProducts() throws JsonProcessingException {
         //give
-        String useKey = "QFJSyeIZbO77";
         String body="{\"intent\":{\"id\":\"652a0a9a27e3c4125a33f6eb\",\"name\":\"상품정보 입력 및 미리보기\"},\"userRequest\":{\"params\":{},\"block\":{\"id\":\"652a0a9a27e3c4125a33f6eb\",\"name\":\"상품정보 입력 및 미리보기\"},\"utterance\":\"등록하기\",\"lang\":\"ko\",\"user\":{\"id\":\"bf1409542da67b26c2262a0a2f72ac6b5df6ad45e49e90543bd4586af560622863\",\"type\":\"botUserKey\",\"properties\":{\"botUserKey\":\"bf1409542da67b26c2262a0a2f72ac6b5df6ad45e49e90543bd4586af560622863\",\"isFriend\":false,\"plusfriendUserKey\":\""+useKey+"\",\"bot_user_key\":\"bf1409542da67b26c2262a0a2f72ac6b5df6ad45e49e90543bd4586af560622863\",\"plusfriend_user_key\":\"QFJSyeIZbO77\"}}},\"bot\":{\"id\":\"65262a2d31101d1cb1106082!\",\"name\":\"중계나라\"},\"action\":{\"name\":\"상품_정보입력_미리보기\",\"clientExtra\":{},\"params\":{\"productName\":\"상품이름\",\"productDescription\":\"상품설명\",\"productImg\":\"{\\\"privacyAgreement\\\":\\\"Y\\\",\\\"imageQuantity\\\":\\\"1\\\",\\\"secureUrls\\\":\\\"List(http://secure.kakaocdn.net/dna/eAWeuk/K6beei5MQR/XXX/img_org.jpg?credential\\u003dKq0eSbCrZgKIq51jh41Uf1jLsUh7VWcz\\u0026expires\\u003d1702641400\\u0026allow_ip\\u003d\\u0026allow_referer\\u003d\\u0026signature\\u003ddI2NZTDK5C4LsQ2u8E2gpmazuPM%3D)\\\",\\\"expire\\\":\\\"2023-12-15T11:56:40+0000\\\"}\",\"productPrice\":\"100000\",\"kakaoOpenChatUrl\":\"https://open.kakao.com/o/slfrEtXf\",\"tradingLocation\":\"부천시\"},\"id\":\"652e18c39df46e7601eb27eb\",\"detailParams\":{}},\"contexts\":[]}";
         chatBotRequest = mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false)
                 .readValue(body, ChatBotRequest.class);
@@ -97,7 +104,6 @@ class ProductServiceTest {
     @Transactional
     void getCustomerProductDetail() throws JsonProcessingException {
         //give
-        String productId = "5942";
         String body="{\"intent\":{\"id\":\"657aa39383cd6b068ef93501\",\"name\":\"회원 등록상품 상세보기 \"},\"userRequest\":{\"params\":{},\"block\":{\"id\":\"657aa39383cd6b068ef93501\",\"name\":\"회원 등록상품 상세보기 \"},\"utterance\":\"상세보기\",\"lang\":\"ko\",\"user\":{\"id\":\"bf1409542da67b26c2262a0a2f72ac6b5df6ad45e49e90543bd4586af560622863\",\"type\":\"botUserKey\",\"properties\":{\"botUserKey\":\"bf1409542da67b26c2262a0a2f72ac6b5df6ad45e49e90543bd4586af560622863\",\"isFriend\":false,\"plusfriendUserKey\":\"QFJSyeIZbO77\",\"bot_user_key\":\"bf1409542da67b26c2262a0a2f72ac6b5df6ad45e49e90543bd4586af560622863\",\"plusfriend_user_key\":\"QFJSyeIZbO77\"}}},\"bot\":{\"id\":\"65262a2d31101d1cb1106082!\",\"name\":\"중계나라\"},\"action\":{\"name\":\"회원_등록상품_상세보기\",\"clientExtra\":{\"extra\":{\"productId\":\"7810\"},\"productId\":\""+productId+"\"},\"params\":{},\"id\":\"657aa3fc83cd6b068ef93510\",\"detailParams\":{}},\"contexts\":[]}";
         chatBotRequest = mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false)
                 .readValue(body, ChatBotRequest.class);
@@ -109,17 +115,14 @@ class ProductServiceTest {
 
         //then
         Assertions.assertThat(title).isEqualTo(product.getName());
-
     }
 
     @Test
     void updateProductStatus() throws JsonProcessingException {
         //give
-        String productId = "5942";
-        ProductStatus productStatus = ProductStatus.판매중;
-        String status = productStatus.name();
+        String utterance =  ProductStatus.판매중.name();
 
-        String body="{\"intent\":{\"id\":\"657aba23eca3c21b078678c3\",\"name\":\"회원 등록상품 상태변경\"},\"userRequest\":{\"params\":{},\"block\":{\"id\":\"657aba23eca3c21b078678c3\",\"name\":\"회원 등록상품 상태변경\"},\"utterance\":\""+status+"\",\"lang\":\"ko\",\"user\":{\"id\":\"bf1409542da67b26c2262a0a2f72ac6b5df6ad45e49e90543bd4586af560622863\",\"type\":\"botUserKey\",\"properties\":{\"botUserKey\":\"bf1409542da67b26c2262a0a2f72ac6b5df6ad45e49e90543bd4586af560622863\",\"isFriend\":false,\"plusfriendUserKey\":\"QFJSyeIZbO77\",\"bot_user_key\":\"bf1409542da67b26c2262a0a2f72ac6b5df6ad45e49e90543bd4586af560622863\",\"plusfriend_user_key\":\"QFJSyeIZbO77\"}}},\"bot\":{\"id\":\"65262a2d31101d1cb1106082!\",\"name\":\"중계나라\"},\"action\":{\"name\":\"회원_등록상품_상태변경\",\"clientExtra\":{\"productId\":\""+productId+"\"},\"params\":{},\"id\":\"657aba4fa678316d2c4af513\",\"detailParams\":{}},\"contexts\":[]}";
+        String body="{\"intent\":{\"id\":\"657aba23eca3c21b078678c3\",\"name\":\"회원 등록상품 상태변경\"},\"userRequest\":{\"params\":{},\"block\":{\"id\":\"657aba23eca3c21b078678c3\",\"name\":\"회원 등록상품 상태변경\"},\"utterance\":\""+utterance+"\",\"lang\":\"ko\",\"user\":{\"id\":\"bf1409542da67b26c2262a0a2f72ac6b5df6ad45e49e90543bd4586af560622863\",\"type\":\"botUserKey\",\"properties\":{\"botUserKey\":\"bf1409542da67b26c2262a0a2f72ac6b5df6ad45e49e90543bd4586af560622863\",\"isFriend\":false,\"plusfriendUserKey\":\"QFJSyeIZbO77\",\"bot_user_key\":\"bf1409542da67b26c2262a0a2f72ac6b5df6ad45e49e90543bd4586af560622863\",\"plusfriend_user_key\":\"QFJSyeIZbO77\"}}},\"bot\":{\"id\":\"65262a2d31101d1cb1106082!\",\"name\":\"중계나라\"},\"action\":{\"name\":\"회원_등록상품_상태변경\",\"clientExtra\":{\"productId\":\""+productId+"\"},\"params\":{},\"id\":\"657aba4fa678316d2c4af513\",\"detailParams\":{}},\"contexts\":[]}";
         chatBotRequest = mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false)
                 .readValue(body, ChatBotRequest.class);
 
@@ -128,17 +131,16 @@ class ProductServiceTest {
         String text = chatBotResponse.getTemplate().getOutputs().get(0).getSimpleText().getText();
 
         //then
-        Assertions.assertThat(text).isEqualTo("상품을 "+status+" 상태로 변경하였습니다.");
+        Assertions.assertThat(text).isEqualTo("상품을 "+utterance+" 상태로 변경하였습니다.");
     }
 
     @Test
     @Transactional
     void deleteProduct() throws JsonProcessingException {
         //give
-        String productId = "5942";
-        String status = "삭제";
+        String utterance = ProductStatus.삭제.name();
 
-        String body="{\"intent\":{\"id\":\"657aba23eca3c21b078678c3\",\"name\":\"회원 등록상품 상태변경\"},\"userRequest\":{\"params\":{},\"block\":{\"id\":\"657aba23eca3c21b078678c3\",\"name\":\"회원 등록상품 상태변경\"},\"utterance\":\""+status+"\",\"lang\":\"ko\",\"user\":{\"id\":\"bf1409542da67b26c2262a0a2f72ac6b5df6ad45e49e90543bd4586af560622863\",\"type\":\"botUserKey\",\"properties\":{\"botUserKey\":\"bf1409542da67b26c2262a0a2f72ac6b5df6ad45e49e90543bd4586af560622863\",\"isFriend\":false,\"plusfriendUserKey\":\"QFJSyeIZbO77\",\"bot_user_key\":\"bf1409542da67b26c2262a0a2f72ac6b5df6ad45e49e90543bd4586af560622863\",\"plusfriend_user_key\":\"QFJSyeIZbO77\"}}},\"bot\":{\"id\":\"65262a2d31101d1cb1106082!\",\"name\":\"중계나라\"},\"action\":{\"name\":\"회원_등록상품_상태변경\",\"clientExtra\":{\"productId\":\""+productId+"\"},\"params\":{},\"id\":\"657aba4fa678316d2c4af513\",\"detailParams\":{}},\"contexts\":[]}";
+        String body="{\"intent\":{\"id\":\"657aba23eca3c21b078678c3\",\"name\":\"회원 등록상품 상태변경\"},\"userRequest\":{\"params\":{},\"block\":{\"id\":\"657aba23eca3c21b078678c3\",\"name\":\"회원 등록상품 상태변경\"},\"utterance\":\""+utterance+"\",\"lang\":\"ko\",\"user\":{\"id\":\"bf1409542da67b26c2262a0a2f72ac6b5df6ad45e49e90543bd4586af560622863\",\"type\":\"botUserKey\",\"properties\":{\"botUserKey\":\"bf1409542da67b26c2262a0a2f72ac6b5df6ad45e49e90543bd4586af560622863\",\"isFriend\":false,\"plusfriendUserKey\":\"QFJSyeIZbO77\",\"bot_user_key\":\"bf1409542da67b26c2262a0a2f72ac6b5df6ad45e49e90543bd4586af560622863\",\"plusfriend_user_key\":\"QFJSyeIZbO77\"}}},\"bot\":{\"id\":\"65262a2d31101d1cb1106082!\",\"name\":\"중계나라\"},\"action\":{\"name\":\"회원_등록상품_상태변경\",\"clientExtra\":{\"productId\":\""+productId+"\"},\"params\":{},\"id\":\"657aba4fa678316d2c4af513\",\"detailParams\":{}},\"contexts\":[]}";
         chatBotRequest = mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false)
                 .readValue(body, ChatBotRequest.class);
 
