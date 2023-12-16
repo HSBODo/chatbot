88a1ea7a0a7ace27bebd3ecb1874000291414a71
HSBODo <vinsulill@gmaile.com>
2023-12-16 22:27:25 +0900

    refactor  : System.setProperty 제거

--- src/main/java/site/pointman/chatbot/ChatbotApplication.java
+++ src/main/java/site/pointman/chatbot/ChatbotApplication.java
@@ -2,7 +2,6 @@ package site.pointman.chatbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -11,11 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class ChatbotApplication {
 
-	static {
-		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(ChatbotApplication.class, args);
 	}
-
 }
