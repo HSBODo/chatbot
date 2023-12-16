30920bd8f4faf4a135df8c2ef0227985623d8cf0
HSBODo <vinsulill@gmaile.com>
2023-12-16 16:46:35 +0900

    feat : 버튼명 enum추가

---
+++ src/main/java/site/pointman/chatbot/constant/ButtonName.java
@@ -0,0 +1,24 @@
+package site.pointman.chatbot.constant;
+
+public enum ButtonName {
+    등록,
+    숨김,
+    취소,
+    예약,
+    예약취소,
+    삭제,
+    판매중,
+    판매완료,
+    이전으로,
+    메인으로,
+    등록하기,
+    상세보기,
+    회원탈퇴,
+    연락처변경,
+    판매내역,
+    회원가입,
+    처음으로,
+
+
+
+}
