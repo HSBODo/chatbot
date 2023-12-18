d9269351ed95da92568dc868c01d36d9ea627dff
HSBODo <vinsulill@gmaile.com>
2023-12-18 14:56:48 +0900

    feat  : member role 추가

--- src/main/java/site/pointman/chatbot/constant/MemberRole.java
+++ src/main/java/site/pointman/chatbot/constant/MemberRole.java
@@ -1,5 +1,35 @@
 package site.pointman.chatbot.constant;
 
+import java.util.Arrays;
+
 public enum MemberRole {
-    ADMIN,CUSTOMER
+    CUSTOMER_BRONZE("브론즈회원",1000L),
+    CUSTOMER_SILVER("실버회원",10000L),
+    CUSTOMER_PLATINUM("플래티넘회원",100000L),
+    CUSTOMER_DIAMOND("다이아몬드회원",1000000L),
+    ADMIN("관리자",Long.MAX_VALUE)
+    ;
+
+    private final String value;
+    private final Long score;
+
+    MemberRole(String value, Long score) {
+        this.value = value;
+        this.score = score;
+    }
+
+    public static MemberRole getRank(Long score){
+        return Arrays.stream(MemberRole.values())
+                .filter(memberRole -> memberRole.getScore() >= score)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(String.format("%s점수 이상의 등급이 존재하지 않습니다.", score)));
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public Long getScore() {
+        return score;
+    }
 }
