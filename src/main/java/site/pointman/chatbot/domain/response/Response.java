33fda5f61293873eeaacad72005bd7353a511be7
HSBODo <vinsulill@gmaile.com>
2023-12-18 10:33:50 +0900

    refactor  : 더미 객체 생성

--- src/main/java/site/pointman/chatbot/domain/response/Response.java
+++ src/main/java/site/pointman/chatbot/domain/response/Response.java
@@ -1,13 +1,5 @@
 package site.pointman.chatbot.domain.response;
 
-import lombok.Getter;
-import lombok.Setter;
-
-import java.util.HashMap;
-import java.util.Map;
-
-@Getter
-@Setter
 public class Response {
-    private Map<Object,Object> response = new HashMap<>();
+
 }
