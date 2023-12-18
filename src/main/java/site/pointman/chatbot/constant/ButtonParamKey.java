401a3dcab4b0d5fffd390ec855d86b229b727468
HSBODo <vinsulill@gmaile.com>
2023-12-19 02:33:46 +0900

    feat : 추가

--- src/main/java/site/pointman/chatbot/constant/ButtonParamKey.java
+++ src/main/java/site/pointman/chatbot/constant/ButtonParamKey.java
@@ -1,5 +1,9 @@
 package site.pointman.chatbot.constant;
 
 public enum ButtonParamKey {
-    productId,choice
+    productId,choice,noticeId
+
+    /**
+     *  key 추가시 ClientExtra 클래스의 맴버변수 추가해야함.
+     */
 }
