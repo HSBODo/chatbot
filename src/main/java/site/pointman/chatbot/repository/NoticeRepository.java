2e908e4aad331431a7d2ee020fc0b3654e1ae29d
HSBODo <vinsulill@gmaile.com>
2023-12-19 03:08:14 +0900

    feat : findByNoticeId 추가

--- src/main/java/site/pointman/chatbot/repository/NoticeRepository.java
+++ src/main/java/site/pointman/chatbot/repository/NoticeRepository.java
@@ -4,9 +4,10 @@ import site.pointman.chatbot.constant.NoticeStatus;
 import site.pointman.chatbot.domain.notice.Notice;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface NoticeRepository {
     Long save(Notice notice);
-    List<Notice> findAll(NoticeStatus noticeStatus);
-
+    List<Notice> findByStatus(NoticeStatus noticeStatus);
+    Optional<Notice> findByNoticeId(Long noticeId);
 }
--- src/main/java/site/pointman/chatbot/repository/impl/NoticeRepositoryImpl.java
+++ src/main/java/site/pointman/chatbot/repository/impl/NoticeRepositoryImpl.java
@@ -7,6 +7,7 @@ import site.pointman.chatbot.repository.NoticeRepository;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 public class NoticeRepositoryImpl implements NoticeRepository {
     private final EntityManager em;
@@ -25,10 +26,18 @@ public class NoticeRepositoryImpl implements NoticeRepository {
     }
 
     @Override
-    public List<Notice> findAll(NoticeStatus noticeStatus) {
-        return em.createQuery("select n from Notice n where n.status =:status OR n.status =:main ORDER BY FIELD(n.status,:main,:status)  ",Notice.class)
+    public List<Notice> findByStatus(NoticeStatus noticeStatus) {
+        return em.createQuery("select n from Notice n where n.status =:status OR n.status =:main ORDER BY FIELD(n.status,:main,:status)",Notice.class)
                 .setParameter("status",noticeStatus)
                 .setParameter("main",NoticeStatus.메인)
                 .getResultList();
+    }
+
+    @Override
+    public Optional<Notice> findByNoticeId(Long noticeId) {
+        return em.createQuery("select n from Notice n where n.status <>:status AND n.id =:id",Notice.class)
+                .setParameter("id",noticeId)
+                .setParameter("status",NoticeStatus.숨김)
+                .getResultList().stream().findAny();
     }
 }
