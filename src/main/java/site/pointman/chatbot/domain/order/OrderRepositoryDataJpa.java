b4550c06f6834cb2379374df8e5e71552fadd72c
HSBODo <vinsulill@gmaile.com>
2023-12-20 17:26:41 +0900

    Remove : 삭제

--- src/main/java/site/pointman/chatbot/domain/order/OrderRepositoryDataJpa.java
+++
@@ -1,9 +0,0 @@
-package site.pointman.chatbot.domain.order;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface OrderRepositoryDataJpa extends JpaRepository<Order,Long> {
-
-
-}
