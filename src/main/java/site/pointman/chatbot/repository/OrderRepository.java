fafc3d29c7cc67720b4af85ecb3f77f4de8e76fe
HSBODo <vinsulill@gmaile.com>
2023-12-20 17:28:43 +0900

    refacotr : 주문 레포지토리 리팩터링

--- src/main/java/site/pointman/chatbot/repository/OrderRepository.java
+++ src/main/java/site/pointman/chatbot/repository/OrderRepository.java
@@ -6,12 +6,5 @@ import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepository {
-    List<Order> findByApproveOrders(String kakaoUserkey);
-    Order savePayReady(Order order);
-    Optional<Order> findByReadyOrder(Long orderId);
-    Optional<Order> findByApproveOrder(Long orderId);
-    Optional<Order> findByOrder(String kakaoUserkey, Long orderId);
-    Optional<Order> updatePayApprove(Order updateParams);
-    Optional<Order> updatePayCancel(Order updateParams);
-    List<Order> findBySalesRank();
+    Long save(Order order);
 }
