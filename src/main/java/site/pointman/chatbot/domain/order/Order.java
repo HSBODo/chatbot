649210a36c15019a5e5a72e13189e8f57f65a4e9
HSBODo <vinsulill@gmaile.com>
2023-12-21 15:17:24 +0900

    feat : getPurchaseProductProfile 메서드 추가

--- src/main/java/site/pointman/chatbot/domain/order/Order.java
+++ src/main/java/site/pointman/chatbot/domain/order/Order.java
@@ -5,11 +5,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import site.pointman.chatbot.constant.OrderStatus;
-import site.pointman.chatbot.constant.PaymentStatus;
 import site.pointman.chatbot.domain.BaseEntity;
 import site.pointman.chatbot.domain.member.Member;
 import site.pointman.chatbot.domain.payment.PaymentInfo;
 import site.pointman.chatbot.domain.product.Product;
+import site.pointman.chatbot.utill.StringUtils;
 
 import javax.persistence.*;
 
@@ -50,5 +50,31 @@ public class Order extends BaseEntity {
 
     public void changeStatus(OrderStatus status){
         this.status = status;
+    }
+
+    public String getPurchaseProductProfile(){
+        StringBuilder productProfile = new StringBuilder();
+        String formatPrice = StringUtils.formatPrice(product.getPrice());
+        return productProfile
+                .append("상품상태: " + product.getStatus().getOppositeValue())
+                .append("\n\n")
+                .append("판매자: " + product.getMember().getName())
+                .append("\n")
+                .append("카테고리: " + product.getCategory().getValue())
+                .append("\n\n")
+                .append("판매가격: " + formatPrice+"원")
+                .append("\n\n")
+                .append("상품 설명: " + product.getDescription())
+                .append("\n\n")
+                .append("거래 희망 장소: " + product.getTradingLocation())
+                .append("\n")
+                .append("카카오 오픈 채팅방: " + product.getKakaoOpenChatUrl())
+                .append("\n\n")
+                .append("결제일자: " + getFormatApproveDate())
+                .toString();
+    }
+
+    public String getFormatApproveDate(){
+       return StringUtils.dateFormat(paymentInfo.getCreateDate(),"yyyy-MM-dd hh:mm:ss","yyyy-MM-dd");
     }
 }
