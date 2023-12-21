515a0e62c2ce954f45723c956736b8ac3e2dfeb3
HSBODo <vinsulill@gmaile.com>
2023-12-21 15:18:25 +0900

    style : import최적화

--- src/main/java/site/pointman/chatbot/service/OrderService.java
+++ src/main/java/site/pointman/chatbot/service/OrderService.java
@@ -1,8 +1,6 @@
 package site.pointman.chatbot.service;
 
-import site.pointman.chatbot.domain.member.Member;
 import site.pointman.chatbot.domain.payment.PaymentInfo;
-import site.pointman.chatbot.domain.product.Product;
 
 public interface OrderService {
     Long addOrder(PaymentInfo paymentInfo);
