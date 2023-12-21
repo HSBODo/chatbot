2e0ffcf9f370c571f1745bea5c3c92e2b9e574b7
HSBODo <vinsulill@gmaile.com>
2023-12-21 15:27:26 +0900

    style : import 최적화

--- src/main/java/site/pointman/chatbot/controller/admin/OrderController.java
+++ src/main/java/site/pointman/chatbot/controller/admin/OrderController.java
@@ -5,8 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import site.pointman.chatbot.domain.payment.kakaopay.KakaoPaymentApproveResponse;
-import site.pointman.chatbot.domain.payment.kakaopay.KakaoPaymentCancelResponse;
 import site.pointman.chatbot.domain.payment.kakaopay.KakaoPaymentReadyResponse;
 import site.pointman.chatbot.service.PaymentService;
 
