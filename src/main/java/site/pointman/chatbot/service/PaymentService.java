b9debccf0dc14a4020aaa08c9b95075dd36ecbaf
HSBODo <vinsulill@gmaile.com>
2023-12-27 15:33:58 +0900

    feat : 결제서비스 추가

--- src/main/java/site/pointman/chatbot/service/PaymentService.java
+++ src/main/java/site/pointman/chatbot/service/PaymentService.java
@@ -1,9 +1,11 @@
 package site.pointman.chatbot.service;
 
+import site.pointman.chatbot.constant.PaymentStatus;
 import site.pointman.chatbot.domain.payment.PaymentInfo;
 import site.pointman.chatbot.domain.payment.kakaopay.KakaoPaymentApproveResponse;
 import site.pointman.chatbot.domain.payment.kakaopay.KakaoPaymentCancelResponse;
 import site.pointman.chatbot.domain.payment.kakaopay.KakaoPaymentReadyResponse;
+import site.pointman.chatbot.domain.response.HttpResponse;
 
 import java.io.UnsupportedEncodingException;
 
@@ -11,4 +13,5 @@ public interface PaymentService {
     KakaoPaymentReadyResponse kakaoPaymentReady(Long productId, String userKey) throws UnsupportedEncodingException;
     KakaoPaymentApproveResponse kakaoPaymentApprove(String pgToken, PaymentInfo paymentReadyInfo);
     KakaoPaymentCancelResponse kakaoPaymentCancel(PaymentInfo successPaymentInfo);
+    HttpResponse getPaymentInfoByStatus(Long orderId, PaymentStatus paymentStatus);
 }
